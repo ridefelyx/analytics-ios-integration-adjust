ae8a94a26aed323e47cfbe3d10216ae117365c28
bas9990 <39779834+bas9990@users.noreply.github.com>
2022-01-12 17:55:26 +0100

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -4,6 +4,12 @@ import PackageDescription
 
 let package = Package(
     name: "Segment_Adjust",
+      products: [
+        .library(
+            name: "Segment_Adjust",
+            targets: ["Pod"]
+        )
+    ]
     dependencies: [
         .package(url: "https://github.com/adjust/ios_sdk", from: "4.29.3"),
         .package(url: "https://github.com/segmentio/analytics-ios", from: "4.1.6")
